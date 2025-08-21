b2206e18877f71af2f246ca99e38dd726886ef97
john halim <john.halem8848@gmail.com>
2025-08-21 23:07:25 +0300

    08-File Processing(2): process_file_content func on ProcessController, chunking, page content and metadata apply processing success/failed signals

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -3,6 +3,7 @@ from .ProjectController import ProjectController
 import os
 from langchain_community.document_loaders import TextLoader
 from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from models import ProcessingEnums
 
 class ProcessController(BaseController):
@@ -31,4 +32,33 @@ class ProcessController(BaseController):
             return PyMuPDFLoader(file_path)
         
         return None
+    
+    def get_file_content(self, file_id: str):
+        loader = self.get_file_loader(file_id = file_id)
+        return loader.load()
+    
+    def process_file_content(self, file_content: list, file_id: str, chunk_size: int= 100, overlap_size: int=20):
+        
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = chunk_size,
+            chunk_overlap = overlap_size,
+            length_function = len,
+        )
+
+        file_content_texts = [
+            req.page_content
+            for req in file_content
+        ]
+
+        file_content_metadata = [
+            req.metadata
+            for req in file_content
+        ]
+
+        chunks = text_splitter.create_documents(
+            file_content_texts,
+            metadatas = file_content_metadata
+        )
+
+        return chunks
 
