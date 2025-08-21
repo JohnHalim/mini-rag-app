0fcb061e7b4e642221837d0f7dd2487c76da86a3
john halim <john.halem8848@gmail.com>
2025-08-21 15:47:29 +0300

    08-File Processning create process data_router, schemes, ProcessingEnums, ProcessController till get_file_loader installing LandChain

--- src/routes/data.py
+++ src/routes/data.py
@@ -6,6 +6,7 @@ from helpers.config import get_settings, Settings
 from controllers import DataController, ProjectController
 from models import ResponseSignal
 import logging
+from .schemes.data import ProcessRequest
 
 logger = logging.getLogger('uvicorn.error')
 
@@ -56,5 +57,35 @@ async def upload_data  (project_id: str, file: UploadFile, app_settings: Setting
         "file_id" : file_id
         }
     )
+
+
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+    file_id = process_request.file_id
+
+    return file_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
