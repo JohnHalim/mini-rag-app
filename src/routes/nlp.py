86dbad64d91c6b4f331cdf22593a6feb66f2d283
john halim <john.halem8848@gmail.com>
2025-10-15 19:15:23 +0300

    completed the index project method

--- src/routes/nlp.py
+++ src/routes/nlp.py
@@ -1,20 +1,76 @@
 from fastapi import APIRouter, FastAPI, Request, status
 from fastapi.responses import JSONResponse
-from schemes.nlp import PushRequest
-from ..models.ProjectModel import ProjectModel
+from routes.schemes.nlp import PushRequest
+from models.ProjectModel import ProjectModel
+from models.ChunkModel import ChunkModel
+from controllers import NLPController
+from models.enums.ResponseEnums import ResponseSignal
+
 import logging
 
 logger = logging.getLogger("uvicorn.error")
 
 
 nlp_router = APIRouter(
-    prefix="api/v1/nlp",
+    prefix="/api/v1/nlp", 
     tags=["api_v1", "nlp"]
-)
+    )
+
 
 @nlp_router.post(path="/index/push/{project_id}")
 async def index_project(request: Request, project_id: str, push_request: PushRequest):
-    
-    project_model = await ProjectModel.create_instance(db_client= request.app.db_client)
-    
+
+    project_model = await ProjectModel.create_instance(db_client=request.app.db_client)
+
+    chunk_model = await ChunkModel.create_instance(db_client=request.app.db_client)
+
     project = await project_model.get_project_or_create_one(project_id=project_id)
+
+    if not project:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"signal": ResponseSignal.PROJECT_NOT_FOUND_ERROR.value},
+        )
+
+    nlp_controller = NLPController(
+        generation_client=request.app.generation_client,
+        embedding_client=request.app.embedding_client,
+        vectordb_client=request.app.vectordb_client,
+    )
+    
+    has_records = True
+    page_no = 1
+    inserted_items_count=0
+    
+    while has_records:
+        
+        page_chunks = chunk_model.get_project_chunks(project_id=project.id, page_no=page_no)
+        if len(page_chunks):
+            page_no += 1
+            
+        if not page_chunks or len(page_chunks) == 0:
+            has_records = False
+            break
+
+        is_inserted = nlp_controller.index_into_vector_db(
+            project=project_model,
+            chunks=page_chunks,
+            do_reset=push_request.do_reset
+        )
+        
+        inserted_items_count += 1
+        
+        if not is_inserted:
+            return JSONResponse(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content={
+                    "signal" : ResponseSignal.INSERT_INTO_VECTORDB_ERROR.value
+                }
+            )
+        
+        return JSONResponse(
+            content={
+                "signal" : ResponseSignal.INSERT_INTO_VECTORDB_SUCCESS.value,
+                "inserted_items_count" : inserted_items_count
+            }
+        )
