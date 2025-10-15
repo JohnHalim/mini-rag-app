796f9baf6c8eb056ac8f45cb2aa24191d7222198
john halim <john.halem8848@gmail.com>
2025-10-15 19:08:36 +0300

    just modified base controller import and instance, the config type: dict

--- src/stores/vectordb/VectorDBProviderFactory.py
+++ src/stores/vectordb/VectorDBProviderFactory.py
@@ -1,11 +1,11 @@
 from .providers import QdrantDBProvider
 from .VectorDBEnums import VectorDBEnums
-from ...controllers.BaseController import BaseController
+from controllers.BaseController import BaseController
 
 class VectorDBProviderFactory():
-    def __init__(self, config: __dict__):
+    def __init__(self, config: dict):
         self.config = config
-        self.base_controller = BaseController
+        self.base_controller = BaseController()
         
     def create (self, provider: str):
         if provider == VectorDBEnums.QDRANT.value:
