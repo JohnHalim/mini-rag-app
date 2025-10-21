c2fca5f9175064f0168cbc0225bdfde699256cee
john halim <john.halem8848@gmail.com>
2025-10-21 21:34:39 +0300

    add pydantic scheme fot search request

--- src/routes/schemes/nlp.py
+++ src/routes/schemes/nlp.py
@@ -8,3 +8,11 @@ class PushRequest(BaseModel):
        only has do_reset --> optional int = 0'''
     
     do_reset: Optional[int] = 0
+    
+    
+class SearchRequest(BaseModel):
+   '''pydantic scheme for search request in nlp router.
+      takes the text, and limit '''
+      
+   text: str
+   limit: Optional[int] = 5
