d82289050a0b239feddd205bfa344e64df4b0e3f
john halim <john.halem8848@gmail.com>
2025-10-29 16:44:39 +0300

    initialized template parser

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
 from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
+from stores.llm.templates.template_parser import TemplateParser
 
 app = FastAPI()
 
@@ -30,6 +31,12 @@ async def startup_span():
     #vector db client:
     app.vectordb_client = vectordb_provider_factory.create(provider=settings.VECTOR_DB_BACKEND)
     app.vectordb_client.connect()
+    
+    # Template Parser (RAG)
+    app.template_parser = TemplateParser(
+        language=settings.PRIMARY_LANG,
+        default_language=settings.DEFAULT_LANG
+    )
 
 
 # @app.on_event("shutdown")
