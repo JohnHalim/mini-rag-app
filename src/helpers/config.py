a15e8b968e1792f3c41bc7a8e9f8f44a456069a0
john halim <john.halem8848@gmail.com>
2025-11-24 15:05:01 +0200

    install&config postgresql db

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -9,9 +9,17 @@ class Settings(BaseSettings):
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHNUK_SIZE: int
+    
+    ########################### DATABASE #############################
 
-    MONGODB_URL: str
-    MONGODB_DATABASE: str
+    POSTGRES_USERNAME: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_MAIN_DATABASE: str
+
+    # MONGODB_URL: str
+    # MONGODB_DATABASE: str
     
     ########################## LLM Config ###########################
 
