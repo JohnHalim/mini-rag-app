f60044f67ad528393634f750ce58d5857a9065de
John Halim <John.halem8848@gmail.com>
2025-12-15 21:24:13 +0200

    recreate db migration with alembic

--- src/models/db_schemes/minirag/alembic/env.py
+++ src/models/db_schemes/minirag/alembic/env.py
@@ -4,6 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 from schemes import SQLAlchemyBase
 
+
 from alembic import context
 
 # this is the Alembic Config object, which provides
